cdd55de092d177a4f64a0046fb44fc236e99afc4
Wirelab <wirelab@wirelab.local>
2020-01-05 15:41:11 +0000

    add basic stuff

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pf-tf-infra
+
+This repository deploys a set of AWS resources using Terraform.
+
+The resources will handle IoT device traffic and data from bespoke things.
---
+++ variables.tf
@@ -0,0 +1,11 @@
+#AWS credentials
+variable "CI_ID" {}
+
+variable "CI_KEY" {}
+
+variable "APPS_ID" {}
+variable "APPS_KEY" {}
+
+variable "NAMESPACE" {
+  default = "notprod"
+}
